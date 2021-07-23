ed1077e569add4656ea7cc686da800f22d62f5f8
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-07-23 19:38:28 +0800

    Added multiplcation table index.js

---
+++ multiplication_table/index.js
@@ -0,0 +1,19 @@
+//## requirement
+
+//Create new index.js file, write program output 9*9 times table. The output is as follows：
+
+
+//1*1=1
+//2*1=2 2*2=4
+//3*1=3 3*2=6 3*3=9
+//……
+//9*1=9 9*2=18 9*3=27 …… 9*9=81
+
+var result = 0;
+for(var i = 1; i <= 9; i++){
+	document.write("<br>");
+  for(var j = 1; j <= i; j++){
+  result = i * j;
+  document.write(j + "*" + i + "=" + result+ "&nbsp;");
+  }
+}
